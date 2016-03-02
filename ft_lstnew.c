ad0ad1fda2f398ed26ab7d7d844384fc2466f834
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-03-02 23:08:03 +0100

    fix lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/28 08:37:53 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/03/02 23:00:14 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/03/02 23:07:49 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,12 @@ t_list		*ft_lstnew(void const *content, size_t content_size)
 	{
 		l->content = ft_memalloc(content_size);
 		if (l->content)
-			ft_memcpy(l->content, content, content_size);
+		{
+			if (content)
+				ft_memcpy(l->content, content, content_size);
+		}
+		else
+			return (NULL);
 	}
 	l->content_size = content_size;
 	return (l);
