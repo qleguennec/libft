c61ab7808b2b73e89bdec4741ee19056e0b031cb
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-06-18 00:57:17 +0200

    fix macros

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/09/25 18:28:07 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/06/17 11:08:28 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/06/18 00:57:15 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@
 # include <wchar.h>
 
 # define COMPARE(a, b)	(((a) > (b)) - ((a) < (b)))
-# define MIN(a,b)		((a <= b) : a ? b)
-# define MAX(a,b)		((a >= b) : a ? b)
+# define MIN(a,b)		((a <= b) ? a : b)
+# define MAX(a,b)		((a >= b) ? a : b)
 # define LEN(x)			(sizeof(x) / sizeof(x[0]))
-# define SIGN(x)		((x < 0) : 1 ? 0)
-# define ABS(x)			((x < 0) : - x ? x)
+# define SIGN(x)		((x < 0) ? 1 : 0)
+# define ABS(x)			((x < 0) ? - x : x)
 
 typedef struct		s_list
 {
