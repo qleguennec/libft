74766d652018289182f2a4a85240a6f90858a919
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-11-09 22:14:38 +0100

    update strstr

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/25 19:05:43 by qle-guen          #+#    #+#             */
-/*   Updated: 2015/12/10 13:19:56 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/11/09 22:14:34 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 char		*ft_strstr(const char *s1, const char *s2)
 {
-	return (ft_strnstr(s1, s2, ft_strlen(s1)));
+	size_t	len1;
+	size_t	len2;
+
+	if (!*s2)
+		return ((char *)s1);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	while (*s1 && len1 >= len2)
+	{
+		if (!ft_memcmp(s1, s2, len2))
+			return ((char *)s1);
+		s1++;
+		len1--;
+	}
+	return (NULL);
 }
