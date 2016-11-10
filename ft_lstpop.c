58b2b9bea80db73e536c05c282450a9c78ceb4ad
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-11-10 18:01:16 +0100

    fix lstpop

--- ft_lstpop.c
+++ ft_lstpop.c
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 17:58:44 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/11/10 18:00:09 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/11/10 18:01:14 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ t_list		*ft_lstpop(t_list **l)
 {
 	t_list	*ret;
 
+	if (!*l)
+		return (NULL);
 	ret = *l;
 	*l = (*l)->next;
 	return (ret);
