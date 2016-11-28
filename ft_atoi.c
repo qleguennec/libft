7d646f8d4a54612eb55ee3e39521bf31723066ad
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-11-28 19:26:43 +0000

    optimize functions

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,42 +6,24 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/26 16:21:19 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/01/06 11:35:04 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/11/28 18:49:20 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	digitlen(char *s)
-{
-	if (!*s || !ft_isdigit(*s))
-		return (0);
-	else
-		return (1 + digitlen(s + 1));
-}
-
-int				ft_atoi(const char *str)
+int				ft_atoi(const char *s)
 {
 	int		n;
 	int		neg;
-	char	*s;
 
-	s = (char *)str;
 	n = 0;
-	neg = 0;
 	while (ft_isspace(*s))
 		s++;
+	neg = *s == '-';
 	if (*s == '-' || *s == '+')
-	{
-		neg = (*s == '-');
 		s++;
-	}
-	while (*s && digitlen(s) > 0)
-	{
-		n += ((*s) - '0') * ft_pow(10, digitlen(s + 1));
-		s++;
-	}
-	if (neg)
-		n = n * (-1);
-	return (n);
+	while (ft_isdigit(*s))
+		n = 10 * n - (*s++ - '0');
+	return (neg ? n : - n);
 }
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,11 +6,51 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/25 10:00:20 by qle-guen          #+#    #+#             */
-/*   Updated: 2015/12/31 02:02:34 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/11/28 19:26:38 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdint.h>
+
+static void	memcpy_carry(unsigned char *d, unsigned char *s, size_t n)
+{
+	if (n & 8)
+	{
+		*(unsigned int *)(d + 0) = *(unsigned int *)(s + 0);
+		*(unsigned int *)(d + 4) = *(unsigned int *)(s + 4);
+		d += 8;
+		s += 8;
+	}
+	if (n & 4)
+	{
+		*(unsigned int *)(d + 0) = *(unsigned int *)(s + 0);
+		d += 4;
+		s += 4;
+	}
+	if (n & 2)
+	{
+		*d++ = *s++;
+		*d++ = *s++;
+	}
+	if (n & 1)
+		*d++ = *s++;
+}
+
+static void	memcpy_aligned(unsigned char *d, unsigned char *s, size_t n)
+{
+	while (n >= 16)
+	{
+		*(unsigned int *)(d + 0) = *(unsigned int *)(s + 0);
+		*(unsigned int *)(d + 4) = *(unsigned int *)(s + 4);
+		*(unsigned int *)(d + 8) = *(unsigned int *)(s + 8);
+		*(unsigned int *)(d + 12) = *(unsigned int *)(s + 12);
+		s += 16;
+		d += 16;
+		n -= 16;
+	}
+	memcpy_carry(d, s, n);
+}
 
 void		*ft_memcpy(void *dest, const void *src, size_t n)
 {
@@ -19,6 +59,16 @@ void		*ft_memcpy(void *dest, const void *src, size_t n)
 
 	d = (unsigned char *)dest;
 	s = (unsigned char *)src;
+	while ((uintptr_t)s % 4 && n)
+	{
+		*d++ = *s++;
+		n--;
+	}
+	if (!((uintptr_t)d % 4))
+	{
+		memcpy_aligned(d, s, n);
+		return (dest);
+	}
 	while (n)
 	{
 		*d++ = *s++;
