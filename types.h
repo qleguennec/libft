e1072b0497e6310e165a0fe4c31227c2e6d957cd
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2017-01-10 13:59:48 +0100

    add macros

--- types.h
+++ types.h
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/26 13:19:18 by qle-guen          #+#    #+#             */
-/*   Updated: 2017/01/09 16:25:27 by qle-guen         ###   ########.fr       */
+/*   Updated: 2017/01/10 13:56:44 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ typedef unsigned long	t_u64;
 typedef long			t_i64;
 
 # ifdef T_U32_V2
+#  define T_V2
 
 typedef struct			s_u32_v2
 {
@@ -38,6 +39,7 @@ typedef struct			s_u32_v2
 # endif
 
 # ifdef T_U32_V4
+#  define T_V4
 
 typedef struct			s_u32_v4
 {
@@ -45,7 +47,26 @@ typedef struct			s_u32_v4
 	t_u32				b;
 	t_u32				c;
 	t_u32				d;
-}						t_u32_v4
+}						t_u32_v4;
+
+#  define V4L(x) ((x).a)
+#  define V4U(x) ((x).b)
+#  define V4R(x) ((x).c)
+#  define V4D(x) ((x).d)
+# endif
+
+# ifdef T_V2
+
+#  define V2(t, a, b) ((t##_v2){a, b})
+#  define SUM2(t, a, b) V2(t, (a).x+(b).x, (a).y+(b).y)
+#  define DOT2(t, a, b) V2(t, (a).x*(b).x, (a).y*(b).y)
+# endif
+
+# ifdef T_V4
+
+#  define V4(t, a, b, c, d) ((t##_v4){a, b, c, d})
+#  define SUM4(t, x, y) V4(t,(x).a+(y).a,(x).b+(y).b,(x).c+(y).c,(x).d+(y).d)
+#  define DOT4(t, x, y) V4(t,(x).a*(y).a,(x).b*(y).b,(x).c*(y).c,(x).d*(y).d)
 # endif
 
 #endif
